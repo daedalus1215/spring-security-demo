dd2473806c1d041123979fc74832e05322835f0c
larry <ladams1776@yahoo.com>
2018-11-27 06:12:47 -0500

    ADding the DispatcherServletInitializer to replace any XML required for wiring the config and route

---
+++ src/main/java/com/springsecurity/demo/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,26 @@
+package com.springsecurity.demo.config;
+
+import org.springframework.lang.Nullable;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.support.AbstractDispatcherServletInitializer;
+
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Nullable
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Nullable
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] { DemoAppConfig.class };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] { "/" };
+    }
+}
