0a9d7eef293b780f5b4a034a7363ea3d057d46cd
larry <ladams1776@yahoo.com>
2018-12-02 19:49:56 -0500

    @Autowiring in the mysql connection property file, then creating a DataSource Bean with comments of intent for code.

--- src/main/java/com/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/springsecurity/demo/config/DemoAppConfig.java
@@ -1,19 +1,33 @@
 package com.springsecurity.demo.config;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.util.logging.Logger;
+
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.springsecurity.demo")
 @PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+    // the data is stored in this variable from the persistence-mysql.properties file.
+    @Autowired
+    private Environment env;
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+
     @Bean
     public ViewResolver viewResolver() {
         InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -22,5 +36,18 @@ public class DemoAppConfig {
         viewResolver.setSuffix(".jsp");
 
         return viewResolver;
+    }
+
+
+    // define a bean for our security datasource
+    @Bean
+    public DataSource securityDataSource() {
+        // create connection pool
+        // set the jdbc driver class
+        // log the connection props
+        // set database connection props
+        // set connection pool props
+
+        return null;
     }
 }
