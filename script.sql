c9f715c628dc99c4eea638a2e51da6a3f94373de
larry <ladams1776@yahoo.com>
2018-12-04 06:27:34 -0500

    bcrypt has been added to the passwords.

--- script.sql
+++ script.sql
@@ -1,6 +1,3 @@
-DROP DATABASE IF EXISTS `spring_security_demo_plaintext`;
-
-CREATE DATABASE IF NOT EXISTS `spring_security_demo_plaintext`;
 USE `spring_security_demo_plaintext`;
 
 #
@@ -9,11 +6,10 @@ USE `spring_security_demo_plaintext`;
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `username` varchar (50) NOT NULL,
-  `password` varchar (50) NOT NULL,
+  `password` char (68) NOT NULL,
   `enabled` tinyint (1) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
 
 
 #
@@ -21,19 +17,18 @@ CREATE TABLE `users` (
 # non-hashed passwords.
 #
 INSERT INTO `users`
-    VALUES
-      ('john', '{noop}test1', 1),
-      ('mary', '{noop}test12', 1),
-      ('suzanne', '{noop}test123', 1);
+VALUES
+  ('john', '{bcrypt}$2a$04$1pxXKQi1IBkZ4icMVMO5GeMONQLzLwSsS75OJKKzyfNNiXh2CLspu', 1),
+  ('mary', '{bcrypt}$2a$04$BlBCLuEQKfVXoTi8w4Z8rudwDeacry.qzOCq7ZX0f37xwBLsooRxq', 1),
+  ('suzanne', '{bcrypt}$2a$04$JugNriszFTVqhTxld5LEee.5OW6C4r2TQode79Wb1V8WkBvVQZS.S', 1);
 
 
-#
-# Table structure for `authorities`
-#
+
+
 DROP TABLE IF EXISTS `authorities`;
 CREATE TABLE `authorities` (
   `username` varchar (50) NOT NULL,
-  `authority` varchar (5) NOT NULL,
+  `authority` varchar (50) NOT NULL,
   UNIQUE KEY `authorities_idx_1` (`username`, `authority`),
   CONSTRAINT `authorities_ibfk_1`
   FOREIGN KEY (`username`)
@@ -46,13 +41,10 @@ CREATE TABLE `authorities` (
 # Inserting data for table `authorities`
 #
 INSERT INTO `authorities`
-    VALUES
-      ('john', 'ROLE_EMPLOYEE'),
-      ('mary', 'ROLE_EMPLOYEE'),
-      ('mary', 'ROLE_MANAGER'),
-      ('suzanne', 'ROLE_EMPLOYEE'),
-      ('suzanne', 'ROLE_MANAGER'),
-      ('suzanne', 'ROLE_ADMIN');
-
-
-
+VALUES
+  ('john', 'ROLE_EMPLOYEE'),
+  ('mary', 'ROLE_EMPLOYEE'),
+  ('mary', 'ROLE_MANAGER'),
+  ('suzanne', 'ROLE_EMPLOYEE'),
+  ('suzanne', 'ROLE_MANAGER'),
+  ('suzanne', 'ROLE_ADMIN');
